defbf532d7cf5fa20782905d423408881de539f8
Brad Coleman <bradcoleman60@gmail.com>
2023-02-24 21:13:07 -0800

    fixes login / register page

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,19 +4,6 @@ const e = require('express');
 
 const {User, Entry, Comment} = require('../../models')
 
-// Get All Users
-// router.get('/', async (req, res) => {
-    
-//     const usersData = await User.findAll({
-//         include: [{model: Entry}, {model: Comment}]
-        
-//     });
-    
-//     return res.json(usersData);
-    
-// })
-
-
 //Register a new user
 
 router.post("/", async (req, res) => {
@@ -34,21 +21,15 @@ router.post("/", async (req, res) => {
 // Log in User
 router.post('/login', async (req, res) => {
     try {
-        // console.log("this is the req.body" ,req.body)
+       
         const userData = await User.findOne({ where: {user_name :req.body.user_name}});
         
-        // console.log("this is the user data", userData)
-
         if(!userData){
             res
                 .status(400)
                 .json({message: 'Incorrect username or password. Try again'});
                 return;
         }
-
-        // const validatePassword = await userData.dataValues.password.checkPassword(req.body.password);
-        // console.log("the password entered:", req.body.password),
-        // console.log("this is the password from mysql:", userData.dataValues.password)
 
         const validatePassword = await bcrypt.compare(req.body.password, userData.dataValues.password)
 
@@ -70,19 +51,13 @@ router.post('/login', async (req, res) => {
         Note, we need to save this to sessions BEFORE we render the homepage
         because the home page rendering requires the id of the logged in user*/
         req.session.save(() => {
-            // console.log("to be saved in session ====>", userData.dataValues.id)
-            // req.session.user_id = userData.dataValues.id;
-            // req.session.logged_in = true;
-            // res.json({user: userData, message:'You are now logged in'});
+           
             res.render('homepage', {
                 logged_in: req.session.logged_in,
               });
             
         });
-        // res.render('homepage', {
-        //     logged_in: req.session.logged_in,
-        //   });
-
+       
     } catch (err) {
         console.log(" This is the error on userRoutes: ======>", err)
         res.status(400).json(err)
