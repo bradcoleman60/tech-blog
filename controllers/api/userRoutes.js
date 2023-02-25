e56f530b9d443a687dd13a4aff8352d9b1825139
Brad Coleman <bradcoleman60@gmail.com>
2023-02-24 20:06:33 -0800

    removes consolelogs and other extraneous non used code

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -5,29 +5,27 @@ const e = require('express');
 const {User, Entry, Comment} = require('../../models')
 
 // Get All Users
-router.get('/', async (req, res) => {
+// router.get('/', async (req, res) => {
     
-    const usersData = await User.findAll({
-        include: [{model: Entry}, {model: Comment}]
+//     const usersData = await User.findAll({
+//         include: [{model: Entry}, {model: Comment}]
         
-    });
+//     });
     
-    return res.json(usersData);
+//     return res.json(usersData);
     
-})
+// })
 
 
-//Create new user
+//Register a new user
 
 router.post("/", async (req, res) => {
-    console.log(req.body)
+    // console.log(req.body)
     try {
         const newUser = req.body;
-        // newUser.password = await bcrypt.hash(req.body.password, 10);
         const userData = await User.create(newUser);
         return res.status(200).json(userData);
-                           
-    
+               
     } catch (err) {
         res.status(400).json(err)
     }
@@ -36,10 +34,10 @@ router.post("/", async (req, res) => {
 // Log in User
 router.post('/login', async (req, res) => {
     try {
-        console.log("this is the req.body" ,req.body)
+        // console.log("this is the req.body" ,req.body)
         const userData = await User.findOne({ where: {user_name :req.body.user_name}});
         
-        console.log("this is the user data", userData)
+        // console.log("this is the user data", userData)
 
         if(!userData){
             res
@@ -49,8 +47,8 @@ router.post('/login', async (req, res) => {
         }
 
         // const validatePassword = await userData.dataValues.password.checkPassword(req.body.password);
-        console.log("the password entered:", req.body.password),
-        console.log("this is the password from mysql:", userData.dataValues.password)
+        // console.log("the password entered:", req.body.password),
+        // console.log("this is the password from mysql:", userData.dataValues.password)
 
         const validatePassword = await bcrypt.compare(req.body.password, userData.dataValues.password)
 
@@ -63,14 +61,16 @@ router.post('/login', async (req, res) => {
             return;
         }
         
-        //This Saves user id and a variable called "logged_in" as true to the session table. 
-
+        
+        // Defines the variables to be saved in the sessions table (cookie)
         req.session.user_id = userData.dataValues.id;
         req.session.logged_in = true;
 
-
+        /*This Saves user id and a variable called "logged_in" as true to the session table  
+        Note, we need to save this to sessions BEFORE we render the homepage
+        because the home page rendering requires the id of the logged in user*/
         req.session.save(() => {
-            console.log("to be saved in session ====>", userData.dataValues.id)
+            // console.log("to be saved in session ====>", userData.dataValues.id)
             // req.session.user_id = userData.dataValues.id;
             // req.session.logged_in = true;
             // res.json({user: userData, message:'You are now logged in'});
